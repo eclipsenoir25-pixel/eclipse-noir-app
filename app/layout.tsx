c4d86504051f61886ed69f800146eef44f1fe28b
Luigi Lo Conte <luigiloconte@Mac.homenet.telecomitalia.it>
2025-11-24 14:36:00 +0100

    Aggiunto logo Eclipse Noir come watermark

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,16 +29,15 @@ export default function RootLayout({
       <body
         className={`${poppins.variable} ${cinzel.variable} bg-black text-neutral-100 antialiased`}
       >
-        {/* Contenitore principale */}
         <div className="relative min-h-screen overflow-hidden bg-black">
-          {/* LOGO sullo sfondo, fisso, leggermente spostato a sinistra */}
+
+          {/* LOGO sullo sfondo */}
           <img
             src="/logo/logo-eclipse.png"
-            alt="Eclipse Noir"
+            alt="Eclipse Noir watermark"
             className="pointer-events-none select-none fixed inset-y-0 left-0 m-auto h-[50vh] opacity-[0.08] -z-10"
           />
 
-          {/* Tutto il contenuto dell'app sopra il logo */}
           <div className="relative z-10">{children}</div>
         </div>
       </body>
